8c8ca5f65510f35db1cb9d961effeb4d26524749
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-04-17 23:24:44 +0200

    Configured Services and Pipeline

--- ASPNET-MVC-Globalization/Program.cs
+++ ASPNET-MVC-Globalization/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddLocalization(opt =>
+{
+    opt.ResourcesPath = "Resources";
+});
 
+builder.Services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix,
+    opt =>
+    {
+        //opt.ResourcesPath = "Resources";
+        
+    }).AddDataAnnotationsLocalization();
 
 builder.Services.AddControllersWithViews();
 
@@ -21,6 +33,14 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+var supportedCultures = new[] { "en", "fr", "es" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
 
 app.MapControllerRoute(
     name: "default",
