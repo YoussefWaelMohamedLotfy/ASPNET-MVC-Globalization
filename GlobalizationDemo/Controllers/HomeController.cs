e3a7e982383b053124ecc45fd1677fd6bd380960
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-04-18 00:29:05 +0200

    Added Controller Localization

---
+++ GlobalizationDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using GlobalizationDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+using System.Diagnostics;
+
+namespace GlobalizationDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHtmlLocalizer<HomeController> _localizer;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IHtmlLocalizer<HomeController> localizer)
+        {
+            _logger = logger;
+            _localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _localizer["HelloWorld"];
+            ViewData["HelloWorld"] = result;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
