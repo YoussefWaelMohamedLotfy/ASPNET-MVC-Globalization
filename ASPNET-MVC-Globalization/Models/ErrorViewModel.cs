e68bd9a5ada3959c6b122b83e4a4e24862766118
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-04-18 03:38:56 +0200

    Remove old project

--- ASPNET-MVC-Globalization/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASPNET_MVC_Globalization.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
