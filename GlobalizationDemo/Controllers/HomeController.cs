5b0b6fd9d6ab3bc5e9af56e4a60081f6d935752c
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-05-21 15:01:42 +0200

    Update Bootstrap to v5.2.0-beta1

--- GlobalizationDemo/Controllers/HomeController.cs
+++ GlobalizationDemo/Controllers/HomeController.cs
@@ -4,40 +4,39 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Localization;
 using System.Diagnostics;
 
-namespace GlobalizationDemo.Controllers
+namespace GlobalizationDemo.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly IHtmlLocalizer<HomeController> _localizer;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger, IHtmlLocalizer<HomeController> localizer)
     {
-        private readonly IHtmlLocalizer<HomeController> _localizer;
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+        _localizer = localizer;
+    }
 
-        public HomeController(ILogger<HomeController> logger, IHtmlLocalizer<HomeController> localizer)
-        {
-            _logger = logger;
-            _localizer = localizer;
-        }
+    public IActionResult Index()
+    {
+        var result = _localizer["HelloWorld"];
+        ViewData["HelloWorld"] = result;
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            var result = _localizer["HelloWorld"];
-            ViewData["HelloWorld"] = result;
-            return View();
-        }
+    [HttpPost]
+    public IActionResult CultureManagement(string cultureName, string returnUrl)
+    {
+        Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(cultureName)),
+            new CookieOptions { Expires = DateTimeOffset.Now.AddDays(30) });
 
-        [HttpPost]
-        public IActionResult CultureManagement(string cultureName, string returnUrl)
-        {
-            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
-                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(cultureName)),
-                new CookieOptions { Expires = DateTimeOffset.Now.AddDays(30) });
+        return LocalRedirect(returnUrl);
+    }
 
-            return LocalRedirect(returnUrl);
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
